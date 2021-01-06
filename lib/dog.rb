72d66fbac1924bac8977812a3d8e2c772b50d7f4
Lucius Fowler <luciusfow@gmail.com>
2021-01-05 21:48:45 -0500

    commit tree d7ed9ae87ead728029747a7c545f9b451067f03e

---
+++ lib/dog.rb
@@ -0,0 +1,12 @@
+class Dog
+    def initialize(name, breed = "Mutt") 
+        @name = name
+        @breed = breed
+    end
+end
+#dang, this had me stumped
+# class Dog
+#     def initialize(name)
+#         @name = name
+#     end
+# end
